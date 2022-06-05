938675b85e7c042e3b9f2ee7cbe40805af1c241b
elliot <elliot@wired.company>
2022-06-05 14:01:22 +0900

    add vercel.json

--- src/app.ts
+++ src/app.ts
@@ -71,3 +71,5 @@ server.on('listening', () => {
   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
   console.log('Listening on ' + bind);
 });
+
+module.exports = app;
